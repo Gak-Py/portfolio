5e8cee62f48ff46a49d57f93afe040fb0e7fcfb8
jipdesign-jp <jipdesigninfo@gmail.com>
2022-05-13 16:07:00 +0900

    fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,12 +20,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ[SECRET_KEY]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+# Falseに設定
+DEBUG = False
+
+# 開発環境ではlocal_settingsが読み込まれDEBUGがTrueになる
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+# 本番環境では環境変数から値を取得する
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # Application definition
